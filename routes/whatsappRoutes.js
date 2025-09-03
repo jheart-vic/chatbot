3a77cd8fe174d7831d35302222aff2f0df9ea27a
jheart-vic <adebowalevictorjheart@gmail.com>
2025-09-03 15:34:23 +0100

    update whatsApp token

--- routes/whatsappRoutes.js
+++ routes/whatsappRoutes.js
@@ -3,7 +3,23 @@ import { handleIncomingMessage } from "../controllers/whatsappController.js";
 
 const router = express.Router();
 
+
 // WhatsApp webhook endpoint
+router.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = process.env.WHATSAPP_VERIFY_TOKEN;
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token && mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("✅ Webhook verified!");
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
 router.post("/webhook", handleIncomingMessage);
 
 export default router;
