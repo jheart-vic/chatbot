1ad3f4c09028a3c97571a330a3f4632f71f97118
jheart-vic <adebowalevictorjheart@gmail.com>
2025-09-09 17:36:56 +0100

    update order

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createOrder, updateStatus, getOrderStatus } from "../controllers/orderController.js";
+import { createOrder, updateStatus } from "../controllers/orderController.js";
 import { requireAdmin } from '../middleware/auth.js'
 
 const router = express.Router();
@@ -7,6 +7,6 @@ const router = express.Router();
 
 router.post("/create", requireAdmin, createOrder);
 router.put("/status/:id", requireAdmin, updateStatus);
-router.get("/status/:id", requireAdmin, getOrderStatus);
+// router.get("/status/:id", requireAdmin, getOrderStatus);
 
 export default router;
